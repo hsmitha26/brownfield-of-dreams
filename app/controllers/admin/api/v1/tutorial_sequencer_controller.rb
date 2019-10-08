3bf775c23bec5a44d4656b05d9ddfb8f7d2daa05
Tyler Schaffer <tpschaffer@gmail.com>
2019-10-08 14:37:49 -0600

    Add rubocop gem and resolve issues (except 2 to resolve later)
    Co-authored-by: Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>

--- app/controllers/admin/api/v1/tutorial_sequencer_controller.rb
+++ app/controllers/admin/api/v1/tutorial_sequencer_controller.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
+# rubocop:disable all
 class Admin::Api::V1::TutorialSequencerController < Admin::Api::V1::BaseController
+# rubocop:enable all
   def update
     tutorial = Tutorial.find(params[:tutorial_id])
     TutorialSequencer.new(tutorial, ordered_video_ids).run!
@@ -8,7 +12,7 @@ class Admin::Api::V1::TutorialSequencerController < Admin::Api::V1::BaseControll
 
   private
 
-    def ordered_video_ids
-      params[:tutorial_sequencer][:_json]
-    end
+  def ordered_video_ids
+    params[:tutorial_sequencer][:_json]
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,19 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
   def new
+    # rubocop:disable all
     @user ||= User.new
+    # rubocop:enable all
   end
 
   def create
     user = User.find_by(email: params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       session[:user_id] = user.id
       redirect_to dashboard_path
     else
-      flash[:error] = "Looks like your email or password is invalid"
+      flash[:error] = 'Looks like your email or password is invalid'
       render :new
     end
   end
@@ -18,5 +22,4 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to root_path
   end
-
 end
