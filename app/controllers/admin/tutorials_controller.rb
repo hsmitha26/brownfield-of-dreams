6b0cc1ad872d72738233adee200dafb7ca687f98
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-10-12 20:57:13 -0600

    adds code to create new tutorial by an admin

--- app/controllers/admin/tutorials_controller.rb
+++ app/controllers/admin/tutorials_controller.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
 
 class Admin::TutorialsController < Admin::BaseController
-  def create; end
-
   def new
     @tutorial = Tutorial.new
   end
+
+  def create
+    new_tutorial = Tutorial.new(new_tutorial_params)
+    new_tutorial.save
+    if new_tutorial.save
+      flash[:success] = 'Successfully added tutorial.'
+      redirect_to tutorial_path(new_tutorial.id)
+    else
+      flash[:error] = 'Tutorial was not added.'
+      redirect_to new_admin_tutorial_path
+    end
+  end
+
 
   def edit
     @tutorial = Tutorial.find(params[:id])
@@ -20,6 +31,9 @@ class Admin::TutorialsController < Admin::BaseController
   end
 
   private
+  def new_tutorial_params
+    params.require(:tutorial).permit(:id,:title, :description, :thumbnail)
+  end
 
   def tutorial_params
     params.require(:tutorial).permit(:tag_list)
