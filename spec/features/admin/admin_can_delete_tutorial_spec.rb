2dd0e2337192ae11b589e0c5374f3e63a26d79be
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-10-13 12:51:25 -0600

    tests to delete tutorial only

--- spec/features/admin/admin_can_delete_tutorial_spec.rb
+++ spec/features/admin/admin_can_delete_tutorial_spec.rb
@@ -3,12 +3,18 @@ require 'rails_helper'
 describe 'An admin visiting the admin dashboard' do
   it 'can delete a tutorial and associated videos' do
     admin = create(:admin)
-    t1, t2 = create_list(:tutorial, 2)
-    tv1 = create(:video, tutorial_id: t1)
-    tv2 = create(:video, tutorial_id: t2)
+    t1, t2, t3 = create_list(:tutorial, 3)
+    t1v1 = create(:video, tutorial_id: t1.id)
+    t2v2 = create(:video, tutorial_id: t2.id)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-    visit '/admin/dashboard'
+    visit admin_dashboard_path
+# save_and_open_page
+    within(first('.admin-tutorial-card')) do
+      click_on 'Destroy'
+    end
 
+    expect(current_path).to eq(admin_dashboard_path)
+    expect(page).to_not have_content(t1.name)
   end
 end
