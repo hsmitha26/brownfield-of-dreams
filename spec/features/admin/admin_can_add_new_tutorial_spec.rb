0ba5a5cc98a65edead2eb5646ecef5731bc06423
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-10-12 20:25:25 -0600

    clicking on New Tutorial generates new form

--- spec/features/admin/admin_can_add_new_tutorial_spec.rb
+++ spec/features/admin/admin_can_add_new_tutorial_spec.rb
@@ -3,20 +3,19 @@ require 'rails_helper'
 describe "An admin visiting the admin dashboard" do
   it "allows admin to add new tutorial" do
     admin = create(:admin)
-    create_list(:tutorial, 2)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
     visit '/admin/dashboard'
-    expect(page).to have_css('.admin-tutorial-card', count: 2)
 
-    save_and_open_page
+    click_on 'New Tutorial'
+    expect(current_path).to eq(new_admin_tutorial_path)
 
+    # And I fill in 'title' with a meaningful name
+    # And I fill in 'description' with a some content
+    # And I fill in 'thumbnail' with a valid YouTube thumbnail
 
   end
 end
     # When I visit '/admin/tutorials/new'
-    # And I fill in 'title' with a meaningful name
-    # And I fill in 'description' with a some content
-    # And I fill in 'thumbnail' with a valid YouTube thumbnail
     # And I click on 'Save'
     # Then I should be on '/tutorials/{NEW_TUTORIAL_ID}'
     # And I should see a flash message that says "Successfully created tutorial."
