52ffec971434ab82025396c02068e9d834bfc8c4
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-10-13 11:36:46 -0600

    adds sad path testing

--- spec/features/admin/admin_can_add_new_tutorial_spec.rb
+++ spec/features/admin/admin_can_add_new_tutorial_spec.rb
@@ -21,4 +21,27 @@ describe "An admin visiting the admin dashboard" do
   end
 
   # sad path testing
+  it "displays a flash message if tutorial was not added" do
+    admin = create(:admin)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    visit '/admin/dashboard'
+
+    click_on 'New Tutorial'
+    expect(current_path).to eq(new_admin_tutorial_path)
+
+    fill_in 'tutorial[title]', with: 'Brownfield of Dreams'
+    fill_in 'tutorial[description]', with: 'Tips to work in a brownfield project.'
+
+    click_on 'Save'
+    expect(page).to have_content('Tutorial was not added.')
+    expect(current_path).to eq(new_admin_tutorial_path)
+
+    fill_in 'tutorial[title]', with: 'Brownfield of Dreams'
+    fill_in 'tutorial[description]', with: 'Tips to work in a brownfield project.'
+    fill_in 'tutorial[thumbnail]', with: 'http://i3.ytimg.com/vi/BotaoeIFrYE/maxresdefault.jpg'
+    click_on 'Save'
+
+    new_tutorial_id = Tutorial.last.id
+    expect(current_path).to eq(tutorial_path(new_tutorial_id))
+  end
 end
