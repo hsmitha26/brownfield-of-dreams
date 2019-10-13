13c60613e48bf853266d5556f68e643a0e1a32e1
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-10-13 13:02:23 -0600

    test passing for admin can delete tutorial and associated videos

--- spec/features/admin/admin_can_delete_tutorial_spec.rb
+++ spec/features/admin/admin_can_delete_tutorial_spec.rb
@@ -1,20 +1,24 @@
 require 'rails_helper'
 
 describe 'An admin visiting the admin dashboard' do
-  it 'can delete a tutorial and associated videos' do
-    admin = create(:admin)
-    t1, t2, t3 = create_list(:tutorial, 3)
-    t1v1 = create(:video, tutorial_id: t1.id)
-    t2v2 = create(:video, tutorial_id: t2.id)
+  before :each do
+    @admin = create(:admin)
+    @t1, @t2, @t3 = create_list(:tutorial, 3)
+    @t1v1 = create(:video, tutorial_id: @t1.id)
+    @t2v2 = create(:video, tutorial_id: @t2.id)
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
     visit admin_dashboard_path
-# save_and_open_page
+  end
+  it 'can delete a tutorial and associated videos' do
     within(first('.admin-tutorial-card')) do
       click_on 'Destroy'
     end
 
     expect(current_path).to eq(admin_dashboard_path)
-    expect(page).to_not have_content(t1.name)
+    expect(page).to_not have_content(@t1.title)
+    expect(@t1.videos.count).to eq(0)
+    expect(page).to have_content(@t2.title)
+    expect(page).to have_content(@t3.title) 
   end
 end
