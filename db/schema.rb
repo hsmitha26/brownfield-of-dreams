73db3db7bcd5302102e99e14058d9d83c6062e0f
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-10-15 12:47:42 -0600

    Adds email_confirmed and confirm_token to Users table
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_14_192135) do
+ActiveRecord::Schema.define(version: 2019_10_15_184619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,8 @@ ActiveRecord::Schema.define(version: 2019_10_14_192135) do
     t.datetime "updated_at", null: false
     t.string "github_token"
     t.string "github_handle"
+    t.boolean "email_confirmed", default: false
+    t.string "confirm_token"
     t.index ["email"], name: "index_users_on_email"
   end
 
