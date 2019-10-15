a6c1a8506250bc557f9e60d09843cb7ba60ab30d
Tyler Schaffer <tpschaffer@gmail.com>
2019-10-15 16:40:26 -0600

    Change syntax for updating the user confirm_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ class User < ApplicationRecord
 
   def set_confirmation_token
     if self.confirm_token.blank?
-      self.confirm_token = SecureRandom.urlsafe_base64.to_s
+      self.update(confirm_token: SecureRandom.urlsafe_base64.to_s)
     end
   end
 
