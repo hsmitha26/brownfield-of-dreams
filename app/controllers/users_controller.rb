f50b7fe3453db78a88318d8efcd371b24b072b2d
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-10-15 13:19:13 -0600

    removes edit action, adds code to create action
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,16 +19,15 @@ class UsersController < ApplicationController
     user = User.create(user_params)
     if user.save
       session[:user_id] = user.id
+      user.set_confirmation_token
+      user.save(validate: false)
+      UserMailer.registration_confirmation(user).deliver_now
       flash[:success] = "Logged in as #{user.first_name}"
       redirect_to dashboard_path
     else
       flash[:error] = 'Username already exists'
       render :new
     end
-  end
-
-  def edit
-    
   end
 
   private
