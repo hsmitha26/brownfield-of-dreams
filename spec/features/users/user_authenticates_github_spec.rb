a5f7319f3976797b3e1ba6624f23da86cdc059e0
Tyler Schaffer <tpschaffer@gmail.com>
2019-10-15 23:18:36 -0600

    Fix github oauth test

--- spec/features/users/user_authenticates_github_spec.rb
+++ spec/features/users/user_authenticates_github_spec.rb
@@ -1,32 +1,42 @@
 require 'rails_helper'
 
 describe "A logged in user: " do
-  xit "can connect to Github via OAuth" do
-    user = create(:user)
+  before(:each) do
+    @json_repos = File.open('./fixtures/repo_data.json')
+    stub_request(:get, "https://api.github.com/user/repos?access_token=#{ENV['example_github_token']}")
+      .to_return(status: 200, body: @json_repos)
 
+    @json_followers = File.open('./fixtures/follower_data.json')
+    stub_request(:get, "https://api.github.com/user/followers?access_token=#{ENV['example_github_token']}")
+      .to_return(status: 200, body: @json_followers)
+
+    @json_following = File.open('./fixtures/following_data.json')
+    stub_request(:get, "https://api.github.com/user/following?access_token=#{ENV['example_github_token']}")
+      .to_return(status: 200, body: @json_following)
+  end
+
+  it "can connect to Github via OAuth" do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(
+      info: {
+        nickname: 'tschaffer1618'
+      },
+      credentials: {
+        token: ENV['example_github_token'],
+      }
+    )
+
+    user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit dashboard_path
 
-    stub_omniauth
     click_on 'Connect to GitHub'
-    user.reload
 
-    expect(user.github_token).to eq('pizza')
-  end
+    expect(current_path).to eq dashboard_path
 
-  def stub_omniauth
-    OmniAuth.config.test_mode = true
-    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
-      provider: 'github',
-      extra: {
-        raw_info: {
-          login: "1234"
-        }
-      },
-      credentials: {
-        token: "pizza",
-      }
-    })
+    expect(page).to_not have_content('Connect to GitHub')
+
+    expect(page).to have_content("My GitHub")
   end
 end
