ff4fb89d7e22c3747b88f8a45c259531580cbacb
Tyler Schaffer <tpschaffer@gmail.com>
2019-10-16 20:43:27 -0600

    Test sad path for adding friends

--- spec/features/users/user_can_add_friends_spec.rb
+++ spec/features/users/user_can_add_friends_spec.rb
@@ -31,6 +31,37 @@ describe 'A user' do
     end
   end
 
+  it 'sends a flash message if the friend could not be added' do
+    user_1 = create(:user, github_token: ENV['example_github_token'])
+    user_2 = create(:user, first_name: 'Name', last_name: 'Test', github_handle: 'StarPerfect')
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+    json_repos = File.open('./fixtures/repo_data.json')
+    stub_request(:get, "https://api.github.com/user/repos?access_token=#{ENV['example_github_token']}")
+      .to_return(status: 200, body: json_repos)
+
+    json_followers = File.open('./fixtures/follower_data.json')
+    stub_request(:get, "https://api.github.com/user/followers?access_token=#{ENV['example_github_token']}")
+      .to_return(status: 200, body: json_followers)
+
+    json_following = File.open('./fixtures/following_data.json')
+    stub_request(:get, "https://api.github.com/user/following?access_token=#{ENV['example_github_token']}")
+      .to_return(status: 200, body: json_following)
+
+    visit dashboard_path
+
+    user_2.destroy
+
+    within(".followers") do
+      click_on "Add Friend"
+    end
+
+    expect(current_path).to eq dashboard_path
+    expect(page).to have_content("Unable to add friend.")
+  end
+
+
   it 'can see friends on the dashboard' do
     user_1 = create(:user, github_token: ENV['example_github_token'])
     user_2 = create(:user, first_name: 'Name', last_name: 'Test', github_handle: 'StarPerfect')
