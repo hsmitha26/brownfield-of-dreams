a1c520a802eeeeeeb438507adf4fe8708cbcfc9e
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-10-17 09:51:46 -0600

    refactors create action and resolves rubocop offense. Down to 7
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -5,13 +5,10 @@ class FriendshipsController < ApplicationController
     friend = User.find_by(github_handle: params[:friend_id])
     if friend
       @friendship = current_user.friendships.create(friend_id: friend.id)
-      if @friendship.save
-        flash[:notice] = 'Added friend.'
-        redirect_to dashboard_path
-      end
+      flash[:notice] = 'Added friend.'
     else
       flash[:notice] = 'Unable to add friend.'
-      redirect_to dashboard_path
     end
+    redirect_to dashboard_path
   end
 end
