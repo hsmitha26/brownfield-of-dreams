cc27da7c5540d415069c5af4b9202bdb2a8dae74
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-10-17 10:00:27 -0600

    adds #check_invited_user to refactor and resolve rubocop offense. Down to 4
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>

--- app/controllers/invitation_controller.rb
+++ app/controllers/invitation_controller.rb
@@ -6,14 +6,20 @@ class InvitationController < ApplicationController
   def create
     service = GithubService.new(current_user)
     invited_user = service.fetch_user(params['GitHub Handle'])
-    if invited_user && invited_user['email']
-      UserMailer.invitation(current_user, invited_user).deliver_now
+    check_invited_user(invited_user)
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def check_invited_user(user)
+    if user && user['email']
+      UserMailer.invitation(current_user, user).deliver_now
       flash[:sucess] = 'Successfully sent invite!'
-    elsif invited_user['login']
+    elsif user['login']
       flash[:error] = 'The invited user does not have a public email address.'
     else
       flash[:error] = 'The invited user does not exist.'
     end
-    redirect_to dashboard_path
   end
 end
